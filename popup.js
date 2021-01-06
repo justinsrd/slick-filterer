abc1adb3112aa12275a90640c53234f3c05009ff
justinsrd <jsrdeluna@gmail.com>
2021-01-05 23:03:44 -0800

    Add compatability for multiple sd stylesheets

--- popup.js
+++ popup.js
@@ -5,20 +5,34 @@
 })();
 
 function init() {
-    const deals = document.querySelectorAll('.frontpage');
+    let deals;
     const DEFAULT_MAX_SLIDER_VAL = 80;
 
     $.get(chrome.extension.getURL('slickfilter.html'), function(data) {
-        document.querySelector('#globalBar .inner').innerHTML += data;
-        $('#slickfilter-input').attr('max', DEFAULT_MAX_SLIDER_VAL);
-        $('#max').val(DEFAULT_MAX_SLIDER_VAL);
-        setMaxSliderValue();
-        $('#slickfilter-input').on('input', hideDeals);
-        $('#max').on('change', setMaxSliderValue);
-        $('#slickfilter-container').css('height', document.getElementById('globalBar').offsetHeight);
+        if (document.querySelector('.top_userbar_container')) { // when user is logged in/out
+            deals = document.querySelectorAll('[data-module-name="Frontpage Deals"] .dealitem:not(.expiredbutton)');
+            document.querySelector('.top_userbar_container').innerHTML += data;
+
+            $('#slickfilter-input').attr('max', DEFAULT_MAX_SLIDER_VAL);
+            $('#max').val(DEFAULT_MAX_SLIDER_VAL);
+            setMaxSliderValue();
+            $('#slickfilter-input').on('input', hideDealsLoggedIn);
+            $('#max').on('change', setMaxSliderValue);
+            $('#slickfilter-container').css('height', document.getElementById('top_userbar').offsetHeight);
+        } else { // when user has not logged in before
+            deals = document.querySelectorAll('.frontpage');
+            document.querySelector('#globalBar .inner').innerHTML += data;
+
+            $('#slickfilter-input').attr('max', DEFAULT_MAX_SLIDER_VAL);
+            $('#max').val(DEFAULT_MAX_SLIDER_VAL);
+            setMaxSliderValue();
+            $('#slickfilter-input').on('input', hideDealsClean);
+            $('#max').on('change', setMaxSliderValue);
+            $('#slickfilter-container').css('height', document.getElementById('globalBar').offsetHeight);
+        }
     });
 
-    function hideDeals(evt) {
+    function hideDealsClean(evt) {
         const input = evt.target.value;
         if (parseInt(input) || input === '0') {
             $('#display-number').html(input);
@@ -40,6 +54,33 @@ function init() {
                     }
                 } catch(e) {
 
+                }
+            }
+        } else { //if non int is entered
+            setMaxSliderValue();
+        }
+    }
+
+    function hideDealsLoggedIn(evt) {
+        const input = evt.target.value;
+        if (parseInt(input) || input === '0') {
+            $('#display-number').html(input);
+            for (let i = 0; i < deals.length; i++) {
+                try {
+                    let dealId = deals[i].dataset.threadid;
+                    let domItem = document.querySelector('[data-threadid="' + dealId + '"]');
+                    let rating = parseInt(domItem.querySelector('.rating').textContent.replace('+', '')); 
+                    domItem.classList.remove('super-hidden-deal');
+                    if (rating < input) {
+                        domItem.classList.add('super-hidden-deal');
+                        if (deals[i].dataset && deals[i].dataset.promotedid) {
+                            document.querySelectorAll('[data-threadid="' + dealId + '"]').forEach(function(item) {
+                                item.classList.add('super-hidden-deal');
+                            });
+                        }
+                    }
+                } catch(e) {
+                    
                 }
             }
         } else { //if non int is entered
